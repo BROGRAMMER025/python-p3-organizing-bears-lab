34264a5d3f072df2808a92a9230bb916850575cf
allan murigi <allan.murigi@student.moringaschool.com>
2024-01-08 02:21:36 +0300

    organizing bears sql n python

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Energetic', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'White', 'Calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'Brown', 'Disciplined', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'Golden', 'Playful', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Green', 'Mischief', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Brown', 'Gentle', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('unnamed', NULL, NULL, NULL, NULL, true);
